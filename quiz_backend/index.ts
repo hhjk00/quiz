588a7da7cdf5a84c3c12ffa644930a6229017cdc
hhjk00 <eunyu0604@gmail.com>
2022-04-07 02:26:11 +0900

    quiz 18

--- quiz_backend/index.ts
+++ quiz_backend/index.ts
@@ -1,7 +1,99 @@
-console.log("타입스크립트를 실행했어요");
-
 import { DataSource } from "typeorm";
-import { Board } from "./Board.postgres";
+import { Product } from "./Product.postgres";
+import { ApolloServer, gql } from "apollo-server";
+
+
+// 1. 타입
+const typeDefs = gql`
+  input CreateProductInput {
+    name: String
+    detail: String
+    price: Int
+  }
+
+  input UpdateProductInput {
+    name: String
+    detail: String
+    price: Int
+  }
+
+  type Product {
+    _id: ID
+    seller: String
+    name: String
+    detail: String
+    price: Int
+  }
+
+
+  type Query {
+    fetchProducts: [Product]
+    fetchProduct(productId: ID) : Product
+      }
+
+  type Mutation {
+    createProduct(seller: String, createProductInput: CreateProductInput!) : String
+    updateProduct(productId: ID, updateProductInput: UpdateProductInput!) : String
+    deleteProduct(productId: ID) : String
+  }
+`;
+
+// 2. API
+const resolvers = {
+  Query: {
+    fetchProduct: async (_:any, args:any ) => {
+      const result = await Product.findOne({where:{_id :args.productId}})
+      return result
+    },
+
+    fetchProducts: async () => {
+      const result = await Product.find( );
+
+      return result;
+    },
+  },
+
+  Mutation : {
+    createProduct: async (_: any, args: any) => {
+      await Product.insert({
+        ...args,
+          ...args.createProductInput,
+      })
+
+      return "상품을 등록했습니다."
+    },
+
+    updateProduct: async (_:any, args:any) => {
+      await Product.update({ _id: args.productId },
+         { name : args.updateProductInput.name,
+          detail : args.updateProductInput.detail,
+          price : args.updateProductInput.price},
+         )
+         return "상품이 업데이트되었습니다"
+
+    },
+
+    deleteProduct: async (_:any, args:any) => {
+      await Product.update({ _id: args.productId }, { deletedAt: new Date()})
+      return "상품이 삭제되었습니다"
+    }
+
+  }
+    
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  cors:true
+});
+
+
+
+
+
+
+
 
 const AppDataSource = new DataSource({
   type: "postgres",
@@ -10,7 +102,7 @@ const AppDataSource = new DataSource({
   username: "postgres",
   password: "postgres2021",
   database: "postgres",
-  entities: [Board],
+  entities: [Product],
   synchronize: true,
   logging: true,
 });
@@ -18,6 +110,9 @@ const AppDataSource = new DataSource({
 AppDataSource.initialize()
   .then(() => {
     console.log("연결성공");
+    server.listen(4000).then(({ url }) => {
+      console.log(`🚀 Server ready at ${url}`);
+    });
   })
   .catch(() => {
     console.log("연결실패");
